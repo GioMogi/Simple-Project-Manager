19e992206337c6bf2144fd6a527fa0da31b94da6
GioMogi <giovannimogi@cynthias-mbp.lan>
2024-06-04 15:36:39 -0700

    folder/file setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,cjs,mjs,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

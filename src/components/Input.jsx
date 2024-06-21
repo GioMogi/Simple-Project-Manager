d4d794f7ae22ac96246ffa578f75bd56aaaeec4c
GioMogi <giovannimogi@cynthias-mbp.lan>
2024-06-20 18:20:36 -0700

    added new project functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({ label, textArea, ...props }) {
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input({ label, textArea, ...props }, ref) {
   const classes =
     "w-full p-1 border-b-2 rounded-sm border-emerald-300 bg-emerald-200 text-emerald-600 focus:outline-none focus:border-emerald-600";
 
@@ -8,10 +10,12 @@ export default function Input({ label, textArea, ...props }) {
         {label}
       </label>
       {textArea ? (
-        <textarea className={classes} {...props} />
+        <textarea ref={ref} className={classes} {...props} />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
-}
+});
+
+export default Input;
